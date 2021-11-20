9516dd9b4c0939991edfb5f88bc1ddc06f351c2f
Andrei Mackenzie <andrei@clypd.com>
2021-11-19 20:59:26 -0500

    Resolve happy path (success with no redirects)

--- resolver.go
+++ resolver.go
@@ -1,6 +1,36 @@
 package adstxt
 
-func Resolve(doer Doer, domain string) (AdsTxt, error) {
-	//TODO: fill in
-	return AdsTxt{}, nil
+import (
+	"errors"
+	"net/http"
+	"net/url"
+)
+
+var errRequestFailed = errors.New("ads.txt request was not successful")
+
+// Resolve requests and parses ads.txt from the provided host
+func Resolve(doer Doer, host string) (AdsTxt, error) {
+	u := url.URL{
+		Scheme: "https",
+		Host:   host,
+		Path:   "/ads.txt",
+	}
+	req, err := http.NewRequest("GET", u.String(), nil)
+	if err != nil {
+		return AdsTxt{}, err
+	}
+	resp, err := doer.Do(req)
+	if err != nil {
+		return AdsTxt{}, errRequestFailed
+	}
+	//TODO: handle at most one redirect
+	if !isHTTPSuccess(resp) {
+		return AdsTxt{}, err
+	}
+	defer resp.Body.Close()
+	return Parse(resp.Body)
+}
+
+func isHTTPSuccess(resp *http.Response) bool {
+	return resp.StatusCode >= 200 && resp.StatusCode < 300
 }
